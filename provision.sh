138f5d118bb1a5093f4b460311deed07f165ab6c
Alexander Petrov <alexonrails@gmail.com>
2021-02-11 16:41:55 +0300

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,3 +1,25 @@
-if [ -f $HOME/security.tar ] then
-				echo "exists"
+#!/bin/bash
+sudo apt update
+sudo apt install -y build-essential tmux zsh
+if [ -f $HOME/security.tar ] ;
+then
+        echo "exists"
+        mkdir -p security
+        mv security.tar security/
+        cd security
+        tar -xf security.tar
+        make all
+fi
+
+#[ -f $HOME/.zshrc ] && source $HOME/.zshrc
+
+[ ! -d $HOME/.rbenv ] && git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+[ ! -d $HOME/.oh-my-zsh ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+[ ! -d $HOME/.fzf ] && git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf && $HOME/.fzf/install
+[ ! -d $HOME/vimfiles ] && git clone --recursive https://github.com/apetrov/vimfiles $HOME/vimfiles && ln -s $HOME/vimfiles $HOME/.vim
+
+if [ ! -d $HOME/dotfiles ] ;
+then
+        git clone git@github.com:apetrov/dotfiles.git $HOME/dotfiles
+        cd $HOME/dotfiles && make all
 fi
