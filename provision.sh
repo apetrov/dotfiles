7f27030d4bd2d2b4e6c047fffbf4d5810fd6e3d8
Alexander Petrov <alexonrails@gmail.com>
2021-02-12 16:35:12 +0300

    Quite

--- provision.sh
+++ provision.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get clean
-sudo apt-get install -y build-essential tmux zsh unzip jq python3-pip
+sudo apt-get update -qq
+sudo apt-get clean -qq
+sudo apt-get install -qq -y build-essential tmux zsh unzip jq python3-pip
 
 [ -f $HOME/security.tar ] && echo "Installing security" && mkdir -p security && mv security.tar security/ && cd security && tar -xf security.tar && make all
 
-[ ! -d $HOME/.rbenv ] && git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+[ ! -d $HOME/.rbenv ] && git clone --quiet https://github.com/rbenv/rbenv.git ~/.rbenv
 
 [ ! -d $HOME/.oh-my-zsh ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-[ ! -d $HOME/.fzf ] && git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf && $HOME/.fzf/install
+[ ! -d $HOME/.fzf ] && git clone --quiet --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf && $HOME/.fzf/install
 
-[ ! -d $HOME/vimfiles ] && git clone --recursive https://github.com/apetrov/vimfiles $HOME/vimfiles && ln -s $HOME/vimfiles $HOME/.vim
+[ ! -d $HOME/vimfiles ] && git clone --quiet --recursive https://github.com/apetrov/vimfiles $HOME/vimfiles && ln -s $HOME/vimfiles $HOME/.vim
 
 [ ! -d $HOME/aws ] && cd $HOME && curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip" &&  unzip awscliv2.zip && sudo ./aws/install
 
-[ ! -d $HOME/dotfiles ] && git clone git@github.com:apetrov/dotfiles.git $HOME/dotfiles && cd $HOME/dotfiles && make all
+[ ! -d $HOME/dotfiles ] && git clone --quiet git@github.com:apetrov/dotfiles.git $HOME/dotfiles && cd $HOME/dotfiles && make all
 
-[ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-z ] && mkdir -p $HOME/.oh-my-zsh/custom/plugins && git clone https://github.com/agkozak/zsh-z $HOME/.oh-my-zsh/custom/plugins/zsh-z
+[ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-z ] && mkdir -p $HOME/.oh-my-zsh/custom/plugins && git clone --quiet https://github.com/agkozak/zsh-z $HOME/.oh-my-zsh/custom/plugins/zsh-z
 
 chsh -s $(which zsh)
